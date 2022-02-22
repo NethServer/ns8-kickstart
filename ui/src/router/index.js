6a3d43a04764a08b15722d7acd9a1013102b8b22
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2022-02-22 15:17:49 +0100

    ui: remove Logs reference

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Status from "../views/Status.vue";
 import Settings from "../views/Settings.vue";
-import Logs from "../views/Logs.vue";
 
 Vue.use(VueRouter);
 
